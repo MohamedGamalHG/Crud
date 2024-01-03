dcd9b4eb3c5bf2087e09e8bf01ee45b17ba2e4f8
Mohamed_Gamal_Hamza <49466286+MohamedGamalHG@users.noreply.github.com>
2024-01-03 12:09:34 +0200

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) {}
+
+        public DbSet<Category> categories { get; set; } = default!;
+        public DbSet<SubCategory> subCategories { get; set; } = default!;
+        public DbSet<Product> products { get; set; } = default!;
+    }
+}
